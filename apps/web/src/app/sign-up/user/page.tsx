c0f6390be1fa1f0971ddb01dd286ae759e747b00
sharonwav <2082023@unai.edu>
2024-11-12 20:11:49 +0700

    feat: fix create user

--- apps/web/src/app/sign-up/user/page.tsx
+++ apps/web/src/app/sign-up/user/page.tsx
@@ -27,7 +27,7 @@ const SignUpUser: React.FC = () => {
                     placeholder="jane@gmail.com"
                   />
                   <button onClick={() => setShowReferralCode(true)} className="absolute right-2 border border-black px-3 py-[2px] rounded-xl text-sm hover:border-b-4 hover:border-r-4">
-                    Next
+                    next
                   </button>
                 </div>
               </div>
@@ -40,7 +40,6 @@ const SignUpUser: React.FC = () => {
             <div className="min-w-min min-h-min border border-black rounded-xl p-2">
               <div className="flex flex-col items-start mt-5">
                 {!isChecked && (
-                  // Input Referral Code hanya muncul jika checkbox belum dicentang
                   <div className="relative flex items-center border border-black rounded-lg px-4 py-2.5 focus-within:border-b-4 focus-within:border-r-4 mt-2">
                     <input
                       type="text"
@@ -49,32 +48,31 @@ const SignUpUser: React.FC = () => {
                     />
                   </div>
                 )}
-
-                {/* Checkbox untuk menyembunyikan referral code */}
+                <div className="w-24 hidden"></div>
                 <div className="flex items-center space-x-2 mt-3">
                   <input
                     type="checkbox"
                     id="skipReferralCheckbox"
                     checked={isChecked}
                     onChange={handleCheckboxChange}
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
+                    className="w-4 h-4 text-black bg-gray-100 border-gray-300 rounded"
                   />
-                  <label htmlFor="skipReferralCheckbox" className="text-sm font-medium text-gray-900">
-                    I don't have a referral code
+                  <label htmlFor="skipReferralCheckbox" className="text-sm font-medium text-gray-900 w-[318px]">
+                    I don't have a referral code trst
                   </label>
                 </div>
-
-                <button onClick={() => setShowReferralCode(false)} className="mt-3 text-sm text-blue-500 underline">
-                  Back
-                </button>
-                <button onClick={() => setShowPassword(true)} className="mt-3 text-sm text-blue-500 underline">
-                  Next
-                </button>
+                <div className="w-full flex items-center justify-between mt-3">
+                  <button onClick={() => setShowReferralCode(false)} className="border border-black px-3 py-[2px] rounded-xl text-sm hover:border-b-4 hover:border-r-4">
+                    back
+                  </button>
+                  <button onClick={() => setShowPassword(true)} className="border border-black px-3 py-[2px] rounded-xl text-sm hover:border-b-4 hover:border-r-4">
+                    next
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         ) : (
-          // Input Password
           <div className="flex flex-col items-center justify-center relative p-5">
             <label className="text-sm border border-black rounded-full px-2 absolute top-3 right-8 z-10 bg-white">Enter your password here</label>
             <div className="min-w-min min-h-min border border-black rounded-xl p-2">
@@ -86,7 +84,6 @@ const SignUpUser: React.FC = () => {
                     placeholder="Password"
                   />
                 </div>
-
                 <label className="text-sm border border-black rounded-full px-2 absolute top-[3.7rem] left-2 z-10 bg-white">Confirm password</label>
                 <div className="relative flex items-center border border-black rounded-lg px-4 py-2.5 focus-within:border-b-4 focus-within:border-r-4 mt-2">
                   <input
@@ -95,7 +92,7 @@ const SignUpUser: React.FC = () => {
                     placeholder="Confirm password"
                   />
                   <button onClick={() => { setShowPassword(false); setShowReferralCode(true); }} className="absolute right-2 border border-black px-3 py-[2px] rounded-xl text-sm hover:border-b-4 hover:border-r-4">
-                    Back
+                    back
                   </button>
                 </div>
               </div>
